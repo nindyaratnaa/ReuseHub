84a871fb9a89b03645ccdcac8997c8660b586159
Nya <nindyaratnaa@gmail.com>
2025-11-05 21:39:32 +0700

    laravel-project

---
+++ reuse-hub/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+    <body class="bg-gray-900 flex items-center justify-center min-h-screen">
+        <h1 class="text-5xl font-extrabold text-blue-400">🎉 Tailwind Aktif!</h1>
+    </body>
+</html>
